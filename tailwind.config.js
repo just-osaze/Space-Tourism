d7d3fb25af0ec611240c7133a6de2c261d4b3c75
just-osaze <osaigbevo.osaz@gmail.com>
2022-01-25 18:21:32 +0100

    developed all technology pages (bg giving slight issue)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,12 @@ module.exports = {
         'desktop-image': "url('../src/home/background-home-desktop.jpg')",
 
         'mobile-crew': "url('../src/crew/background-crew-mobile.jpg')",
-        'tablet-crew': "url('../src/crew/background-crew-desktop.jpg')",
-        'desktop-crew': "url('../src/crew/background-crew-desktop.jpg')",
+        'tablet-crew': "url('./src/crew/background-crew-tablet.jpg')",
+        'desktop-crew': "url('./src/crew/background-crew-desktop.jpg')",
+
+        'mobile-technology': "url('../src/technology/background-technology-mobile.jpg')",
+        'tablet-technology': "url('../src/technology/background-technology-tablet.jpg')",
+        'desktop-technology': "url('../src/technology/background-technology-desktop.jpg')",
         }), 
       },
   },
